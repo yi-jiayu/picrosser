1945658ab5bbc0e3d1718464a0c556f018abc9da
Jiayu Yi <yijiayu@gmail.com>
2019-10-27 14:47:44 +0800

    Use template matching to determine number

--- find_number.py
+++ find_number.py
@@ -1,64 +1,39 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
-from skimage import data, io, img_as_bool
+import numpy as np
+from skimage import io, img_as_bool
 from skimage.feature import match_template
 
-image = io.imread('data/edges/0295.png')
+
+def find_number(tmpl, im, ax):
+    result = match_template(im, tmpl)
+    ij = np.unravel_index(np.argmax(result), result.shape)
+    x, y = ij[::-1]
+
+    ax.imshow(im, cmap=plt.cm.gray)
+    ax.set_axis_off()
+    # highlight matched region
+    h, w = tmpl.shape
+    rect = plt.Rectangle((x, y), w, h, edgecolor='r', facecolor='none')
+    ax.add_patch(rect)
+    offset = (x + w // 2) - (im.shape[1] // 2)
+    return np.max(result), np.abs(offset)
+
+
+image = io.imread('data/edges/0904.png')
 image = img_as_bool(image)
 
-image2 = io.imread('data/edges/0068.png')
-image2 = img_as_bool(image2)
+fig, axes = plt.subplots(nrows=11, ncols=2, figsize=(4, 22))
 
-image3 = io.imread('data/edges/0405.png')
-image3 = img_as_bool(image3)
+for i in range(0, 22, 2):
+    ax1 = axes[np.unravel_index(i, (11, 2))]
 
-template = io.imread('3.png')
-template = img_as_bool(template)
+    template = io.imread(f'templates/{i // 2 + 1}.png')
+    template = img_as_bool(template)
+    ax1.imshow(template, cmap=plt.cm.gray)
+    ax1.set_axis_off()
 
-result = match_template(image, template)
-print(np.max(result))
-ij = np.unravel_index(np.argmax(result), result.shape)
-x, y = ij[::-1]
-
-fig, axes = plt.subplots(nrows=1, ncols=4)
-ax1, ax2, ax3, ax4 = axes
-
-ax1.imshow(template, cmap=plt.cm.gray)
-ax1.set_axis_off()
-
-ax2.imshow(image, cmap=plt.cm.gray)
-ax2.set_axis_off()
-ax2.set_title(format(np.max(result), '.4f'))
-# highlight matched region
-w, h = template.shape
-rect = plt.Rectangle((x, y), h, w, edgecolor='r', facecolor='none')
-ax2.add_patch(rect)
-
-result = match_template(image2, template)
-print(np.max(result))
-ij = np.unravel_index(np.argmax(result), result.shape)
-x, y = ij[::-1]
-
-ax3.imshow(image2, cmap=plt.cm.gray)
-ax3.set_axis_off()
-ax3.set_title(format(np.max(result), '.4f'))
-# highlight matched region
-w, h = template.shape
-rect = plt.Rectangle((x, y), h, w, edgecolor='r', facecolor='none')
-ax3.add_patch(rect)
-
-result = match_template(image3, template)
-print(np.max(result))
-ij = np.unravel_index(np.argmax(result), result.shape)
-x, y = ij[::-1]
-
-ax4.imshow(image3, cmap=plt.cm.gray)
-ax4.set_axis_off()
-ax4.set_title(format(np.max(result), '.4f'))
-# highlight matched region
-w, h = template.shape
-rect = plt.Rectangle((x, y), h, w, edgecolor='r', facecolor='none')
-ax4.add_patch(rect)
+    ax2 = axes[np.unravel_index(i + 1, (11, 2))]
+    score, offset = find_number(template, image, ax2)
+    ax2.set_title(f'{i}: {score:.3}, {offset}')
 
 plt.show()
