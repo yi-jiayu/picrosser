cfcff7de73f86256f9995961f33ee58c02a2e21f
Jiayu Yi <yijiayu@gmail.com>
2019-10-27 14:54:30 +0800

    Detect best circle

--- find_circle.py
+++ find_circle.py
@@ -9,21 +9,20 @@ import numpy as np
 RADII = np.arange(31, 36)
 
 
-def find_circles(im):
+def find_circles(im, ax):
     edges = canny(im)
     hough_res = hough_circle(edges, RADII)
 
     # Select the most prominent 3 circles
-    accums, cx, cy, radii = hough_circle_peaks(hough_res, RADII, total_num_peaks=3)
-    print(list(zip(accums, radii)))
+    [score], [cx], [cy], [radius] = hough_circle_peaks(hough_res, RADII, total_num_peaks=1)
 
     # Draw them
     im = img_as_ubyte(color.gray2rgb(im))
-    for center_y, center_x, radius in zip(cy, cx, radii):
-        circy, circx = circle_perimeter(center_y, center_x, radius, shape=im.shape)
-        im[circy, circx] = (220, 20, 20)
+    circy, circx = circle_perimeter(cy, cx, radius, shape=im.shape)
+    im[circy, circx] = (220, 20, 20)
 
-    return im
+    ax.imshow(im, cmap=plt.cm.gray)
+    ax.set_title(f'{score:.3} (radius: {radius})')
 
 
 connected = io.imread('data/hints/0015.png')
@@ -33,9 +32,9 @@ fig4 = io.imread('data/hints/0041.png')
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
 
-axes[0, 0].imshow(find_circles(connected), cmap=plt.cm.gray)
-axes[0, 1].imshow(find_circles(disconnected), cmap=plt.cm.gray)
-axes[1, 0].imshow(find_circles(fig3), cmap=plt.cm.gray)
-axes[1, 1].imshow(find_circles(fig4), cmap=plt.cm.gray)
+find_circles(connected, axes[0, 0])
+find_circles(disconnected, axes[0, 1])
+find_circles(fig3, axes[1, 0])
+find_circles(fig4, axes[1, 1])
 
 plt.show()
